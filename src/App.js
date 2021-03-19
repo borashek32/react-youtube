469195c613215c2038f7b195f3ec24254ff29f50
nataly baranova <72130701+borashek32@users.noreply.github.com>
2021-03-19 12:51:30 +0300

    can't finish this course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from "./Todo/TodoList";
+import Context from './context';
+import AddTodo from "./Todo/AddTodo";
+
+const AddTodo = React.lazy(
+    () =>
+        new Promise(resolve => {
+            setTimeout(() => {
+                resolve(import('./Todo/AddTodo'))
+            }, 3000)
+        })
+)
 
 function App() {
+    let [todos, setTodos] = React.useState([])
+
+function toggleTodo(id) {
+    setTodos(
+    todos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed
+        }
+        return todo
+    })
+  )
+}
+
+function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+}
+
+function addTodo(title) {
+    setTodos(todos.concat([
+        {
+        title,
+        id: Date.now(),
+        completed: false,
+    }
+    ]))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='wrapper'>
+        <Context.Provider value={{removeTodo: removeTodo}}>
+          <h1>React tutorial</h1>
+          <AddTodo onCreate={addTodo}/>
+            {todos.length ? (
+                <TodoList todos={todos} onToggle={toggleTodo} />
+            ) : (
+                <p>No todos!</p>
+            )}
+
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        </Context.Provider>
+      </div>
+  )
 }
 
 export default App;
